16704c166dd3056f9a74602c1d78afdd24bee6e7
Artem <artem.bond.2000@mail.ru>
2025-03-31 19:37:33 +0300

    New izmeneniya

--- Puzzle/main.cpp
+++ Puzzle/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     GameBoard model;
+    qmlRegisterType<GameBoard> ("Game", 1, 0, "GameBoardModel");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
